319f46d3a16c27fb92b28087d28d56159b2c8db1
atamakahere <git@atamaka.maskmy.id>
2023-11-02 22:44:47 +0530

    update readme and rename summary.md to SUMMARY.md

--- make.sh
+++ make.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Loop from 0 to 75
 for i in {0..75}; do
 	# Create folder with leading zeros if needed
 	day_number=$(printf "%02d" "$i")
 	mkdir -p "day_$day_number"
-	touch "day_$day_number/summary.md"
+	touch "day_$day_number/SUMMARY.md"
 done
 
 touch "README.md"
