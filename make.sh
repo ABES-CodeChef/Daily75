568e8a5f901ff59dab24809eb1bcce1ed69b4eb1
Tanveer Raza <tanveerraza789@gmail.com>
2023-11-02 22:53:17 +0530

    add clean operation to make script

--- make.sh
+++ make.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-for i in {0..75}; do
-	# Create folder with leading zeros if needed
-	day_number=$(printf "%02d" "$i")
-	mkdir -p "day_$day_number"
-	touch "day_$day_number/SUMMARY.md"
-done
-
-touch "README.md"
+if [ "$1" == "form" ]; then
+	for i in {0..75}; do
+		day_number=$(printf "%02d" "$i")
+		mkdir -p "day_$day_number"
+		touch "day_$day_number/SUMMARY.md"
+	done
+	echo "Form operation completed."
+elif [ "$1" == "clean" ]; then
+	for i in {0..75}; do
+		day_number=$(printf "%02d" "$i")
+		rm -rf "day_$day_number"
+	done
+	echo "Clean operation completed."
+else
+	echo "Usage: $0 [form|clean]"
+	exit 1
+fi
